3101584bda8253bc4592337fbf621e3415688911
unknown <huyphan2792005@gmail.com>
2025-06-21 09:26:33 +0700

    push

--- quiz/src/main/java/com/mycompany/huy/quiz/PrimaryController.java
+++ quiz/src/main/java/com/mycompany/huy/quiz/PrimaryController.java
@@ -1,12 +1,20 @@
 package com.mycompany.huy.quiz;
 
+import java.io.IOException;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 
 
 public class PrimaryController {
-    public void handleQM(ActionEvent event){
-        MyAlert.getInstance().showMsg("Xin chao");
+    public void handleQM(ActionEvent event) throws IOException{
+        Scene scene = new Scene(new FXMLLoader(App.class.getResource("question.fxml")).load());
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("quizz app");
+        stage.show();
     }
     public void handlePra(ActionEvent event){
         MyAlert.getInstance().showMsg("Xin chao, thi nhe");
