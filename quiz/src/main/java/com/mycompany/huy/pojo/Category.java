da2c9ef8e0bac33f9b906ebda326b164b122d0b3
unknown <huyphan2792005@gmail.com>
2025-06-21 10:08:37 +0700

    a

---
+++ quiz/src/main/java/com/mycompany/huy/pojo/Category.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.huy.pojo;
+
+/**
+ *
+ * @author admin
+ */
+public class Category {
+    private int id;
+    
+}
