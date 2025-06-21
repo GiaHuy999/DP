c93db614627109c1ebd9a0d002d699d2a6ba43f4
unknown <huyphan2792005@gmail.com>
2025-06-21 10:25:25 +0700

    a

--- quiz/src/main/java/com/mycompany/huy/quiz/QuestionController.java
+++ quiz/src/main/java/com/mycompany/huy/quiz/QuestionController.java
@@ -4,9 +4,18 @@
  */
 package com.mycompany.huy.quiz;
 
+import com.mycompany.huy.pojo.Category;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 
 /**
  * FXML Controller class
@@ -15,12 +24,31 @@ import javafx.fxml.Initializable;
  */
 public class QuestionController implements Initializable {
 
+    @FXML private ComboBox<Category> cbCates;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        try {
+            // B1 nap driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            //B2 Mo ket noi
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/quizdb", "root", "root");
+            
+            //B3 xu ly truy van
+            Statement stm = conn.createStatement();
+            stm.executeQuery("SELECT * FROM");
+            
+            //B4 Dong ket noi
+            conn.close();
+            
+        } catch (ClassNotFoundException | SQLException ex) {
+            ex.printStackTrace();
+        }
+        
     }    
     
 }
--- quiz/src/main/java/module-info.java
+++ quiz/src/main/java/module-info.java
@@ -2,6 +2,9 @@ module com.mycompany.huy.quiz {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires lombok;
+    requires java.sql;
+
 
     opens com.mycompany.huy.quiz to javafx.fxml;
     exports com.mycompany.huy.quiz;
